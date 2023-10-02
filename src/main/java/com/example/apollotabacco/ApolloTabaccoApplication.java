99f7810741ff1b3cb6a66320d8b6893993a30c5a
Duranna66 <ivan.kushnarev.04@list.ru>
2023-10-02 15:47:20 +0300

    start

---
+++ src/main/java/com/example/apollotabacco/ApolloTabaccoApplication.java
@@ -0,0 +1,13 @@
+package com.example.apollotabacco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloTabaccoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApolloTabaccoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apollotabacco/ApolloTabaccoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apollotabacco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApolloTabaccoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
